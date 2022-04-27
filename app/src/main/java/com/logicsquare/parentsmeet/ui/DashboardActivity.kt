3bd4d3fc0243cb99202cdabb8c450391d14298c7
payalarora <payal@logic-square.com>
2022-04-27 15:00:01 +0530

    Working on Settings

--- app/src/main/java/com/logicsquare/parentsmeet/ui/DashboardActivity.kt
+++ app/src/main/java/com/logicsquare/parentsmeet/ui/DashboardActivity.kt
@@ -2,7 +2,10 @@ package com.logicsquare.parentsmeet.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.logicsquare.parentsmeet.R
 import com.logicsquare.parentsmeet.databinding.ActivityDashboardBinding
+import com.logicsquare.parentsmeet.ui.fragments.SettingsFragment
 
 class DashboardActivity : AppCompatActivity(){
     lateinit var binding:ActivityDashboardBinding
@@ -10,6 +13,38 @@ class DashboardActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.navigationView.menu.getItem(0).isCheckable=false
+        loadFragment(SettingsFragment())
+        setListeners()
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
+
+    private fun setListeners() {
+        binding.navigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_mommy -> {
+                    binding.navigationView.menu.getItem(0).isCheckable=true
+                }
+                R.id.menu_calender -> {
+
+                }
+                R.id.menu_kids -> {
+
+                }
+                R.id.menu_meet -> {
+
+                }
+                R.id.menu_messages -> {
+
+                }
+            }
+            true
+        }
     }
 
 }
---
+++ app/src/main/java/com/logicsquare/parentsmeet/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,27 @@
+package com.logicsquare.parentsmeet.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.logicsquare.parentsmeet.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private lateinit var mBinding: FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        setListeners()
+        return mBinding.root
+    }
+
+    private fun setListeners() {
+    }
+
+
+}
